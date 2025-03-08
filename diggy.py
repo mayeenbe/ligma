2bbe10d9f4e95d954c36dd3c1cc9bee3327e063a
mayeenbe <mayeenmahir@gmail.com>
2025-03-08 19:13:50 +0600

    Add files via upload

---
+++ diggy.py
@@ -0,0 +1,22 @@
+a = 10
+b = 12
+c = 0
+
+if a and b and c:
+    print("all the numbers have boolean value as true")
+else:
+    print("atleast one number has boolean value as false")
+
+
+
+
+
+
+a = 10
+b = 10 
+c = 10
+
+if a > 0 OR b > 0:
+    print("Either of the number is greater than 0")
+else:
+    print("no number is greater than zero")
